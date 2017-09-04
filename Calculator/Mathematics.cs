d5e06b445711ecdf2c9693f44090e7c38481b8f2
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-09-04 14:10:21 +0200

    Oprettet forskellige metoder, samt Teste for metoderne

---
+++ Calculator/Mathematics.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedCalculator
+{
+    public class Mathematics
+    {
+        //public double A { get; set; }
+        //public double B { get; set; }
+
+
+        //plus Metode plusser 2 Doubles
+        public double plus(double a, double b)
+        {
+            double sum;
+            sum = a + b;
+            return sum;
+        }
+
+        //Minus - Minusser 2 doubles
+        public double Minus(double a, double b)
+        {
+            double sum;
+            sum = a - b;
+            return sum;
+        }
+
+        //Gange - Ganger 2 Doubles
+        public double Multi(double a, double b)
+        {
+            double sum;
+            sum = a * b;
+            return sum;
+        }
+
+        //Divider - Dividere 2 doubles
+        public double Divide(double a, double b)
+        {
+            double sum;
+            sum = a / b;
+            return sum;
+        }
+
+        //^2 - Ganger en double med sig selv
+        public double iAnden(double a)
+        {
+            double sum;
+            sum = a * a;
+            return sum;
+        }
+    }
+}
---
+++ CalculatorTests/MathematicsTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedCalculator;
+
+namespace SharedCalculator.Tests
+{
+    [TestClass()]
+    public class MathematicsTests
+    {
+        [TestMethod()]
+        public void plusTest()
+        {
+            //arrange
+            Mathematics TestMath = new Mathematics();
+            //act
+            double actual = TestMath.plus(2, 4);
+            //assert'
+            Assert.AreEqual(6, actual);
+        }
+
+        [TestMethod()]
+        public void MinusTest()
+        {
+            //arrange
+            Mathematics TestMath = new Mathematics();
+            //act
+            double actual = TestMath.Minus(4, 2);
+            //assert'
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestMethod()]
+        public void MultiTest()
+        {
+            //arrange
+            Mathematics TestMath = new Mathematics();
+            //act
+            double actual = TestMath.Multi(2, 4);
+            //assert'
+            Assert.AreEqual(8, actual);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            //arrange
+            Mathematics TestMath = new Mathematics();
+            //act
+            double actual = TestMath.Divide(4, 2);
+            //assert'
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestMethod()]
+        public void iAndenTest()
+        {
+            //arrange
+            Mathematics TestMath = new Mathematics();
+            //act
+            double actual = TestMath.iAnden(2);
+            //assert'
+            Assert.AreEqual(4, actual);
+        }
+    }
+}
